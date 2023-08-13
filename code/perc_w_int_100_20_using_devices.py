44794bbf4c1680aba4e622e110a5b6954e573ad3
Alan <ahw9f@virginia.edu>
2023-08-13 15:33:32 -0400

    two measures remaining

--- code/perc_w_int_100_20_using_devices.py
+++ code/perc_w_int_100_20_using_devices.py
@@ -4,16 +4,38 @@ import pathlib
 import settings
 import os
 import scipy
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+
 
 MEASURE_NAME = "perc_w_int_100_20_using_devices"
 
 
 def generate_higher_geo(df, geoid_length=11):
+    """
+    Instead of having each geography weight the same, we instead calculate the population-weighted probabilities for higher regions
+    """
+
     region_type = settings.FIPS_LENGTH_DESIGNATION[geoid_length]
     df[region_type] = df["geoid"].str[:geoid_length]
-    cdf = (
-        df.groupby([region_type, "year"])["value"].agg("mean").to_frame().reset_index()
-    )
+
+    dfs = []
+    for rt in df[region_type].unique():
+        pdf = df[df[region_type] == rt].copy()
+        pdf["weighted_pop"] = (pdf["B28002_001E"] - pdf["B28002_013E"]) / (
+            pdf["B28002_001E"].sum() - pdf["B28002_013E"].sum()
+        )
+        pdf["value"] = (
+            (pdf["upload_prob"] * pdf["download_prob"]) * pdf["weighted_pop"] * 100
+        )
+        assert pdf["weighted_pop"].sum() - 1 <= 0.000001, print(
+            pdf["weighted_pop"].sum()
+        )  # the weights should sum to one
+        dfs.append(pdf)
+    df = pd.concat(dfs)
+
+    cdf = df.groupby([region_type, "year"])["value"].agg("sum").to_frame().reset_index()
     cdf["measure"] = MEASURE_NAME
     # rename the census tract back into geoid to concat
     cdf = cdf.rename(columns={region_type: "geoid"})
@@ -22,11 +44,36 @@ def generate_higher_geo(df, geoid_length=11):
     return cdf
 
 
+def calculate_cpdf(df, value_col="value", cutoff=25, tail=True):
+    # Frequency
+    stats_df = (
+        df.groupby(value_col)[value_col]
+        .agg("count")
+        .pipe(pd.DataFrame)
+        .rename(columns={value_col: "frequency"})
+    )
+
+    if stats_df.empty:
+        return None
+
+    # PDF
+    stats_df["pdf"] = stats_df["frequency"] / sum(stats_df["frequency"])
+
+    # CDF
+    stats_df["cdf"] = stats_df["pdf"].cumsum()
+    stats_df = stats_df.reset_index()
+
+    if tail:
+        return 1 - stats_df[stats_df[value_col] >= cutoff].min()["cdf"]
+    else:
+        return stats_df[stats_df[value_col] >= cutoff].min()["cdf"]
+
+
 def perc_w_int_100_20_using_devices():
     export_dir = (
         "../data/Accessibility/Percent Fast (internet-connected)/data/distribution"
     )
-    bbn_files = sorted(pathlib.Path(settings.BBN_DATA_DIR).glob("*.csv.xz"))
+    ookla_files = sorted(pathlib.Path(settings.OOKLA_DATA_DIR).glob("*.csv.xz"))
     acs_1_files = sorted(
         pathlib.Path("../sdc.broadband.acs/data/distribution/B28002_001").glob(
             "*.csv.xz"
@@ -39,7 +86,7 @@ def perc_w_int_100_20_using_devices():
     )
 
     overlapped_filenames = sorted(
-        set([f.name for f in bbn_files]).intersection(
+        set([f.name for f in ookla_files]).intersection(
             set([f.name for f in acs_1_files]).intersection(
                 set([f.name for f in acs_13_files])
             )
@@ -47,64 +94,66 @@ def perc_w_int_100_20_using_devices():
     )
 
     # Filter for only overlapping files between the data sets
-    bbn_files = [f for f in bbn_files if f.name in overlapped_filenames]
+    ookla_files = [f for f in ookla_files if f.name in overlapped_filenames]
     acs_1_files = [f for f in acs_1_files if f.name in overlapped_filenames]
     acs_13_files = [f for f in acs_13_files if f.name in overlapped_filenames]
 
-    pbar = tqdm(zip(bbn_files, acs_1_files, acs_13_files))
-    for bbn_f, acs1_f, acs_13_f in pbar:
-        bbn_df = pd.read_csv(bbn_f, dtype={"GEOID20": object})
-        # Preparing bbn file; remove all not nulls but also the specific none-standard values
-        bbn_df = bbn_df[(bbn_df["price"] >= 0) & (bbn_df["price"].notnull())].copy()
-        bbn_df = bbn_df.rename(columns={"GEOID20": "geoid"})
-        bbn_df["speed_value"] = bbn_df["speed"].apply(
-            lambda x: int(x.split(" Mbps")[0])
-        )
-        bbn_df_d_100 = bbn_df[
-            (bbn_df["down_up"] == "Download") & (bbn_df["speed_value"] >= 100)
-        ]
-        bbn_df_u_20 = bbn_df[
-            (bbn_df["down_up"] == "Upload") & (bbn_df["speed_value"] >= 20)
-        ]
-        bdf = pd.merge(bbn_df_d_100, bbn_df_u_20, on=["name", "geoid"], how="inner")
+    pbar = tqdm(zip(ookla_files, acs_1_files, acs_13_files))
+    for ookla_f, acs1_f, acs13_f in pbar:
+        ookla_df = pd.read_csv(ookla_f, dtype={"GEOID20": object})
+        ookla_df = ookla_df.dropna()
+        ookla_df = ookla_df.rename(columns={"GEOID20": "geoid"})
 
-        bdf = (
-            bdf.groupby(["geoid", "year_parsed"])["price"]
+        acs1_df = pd.read_csv(acs1_f, dtype={"GEOID21": object})
+        acs1_df = acs1_df.rename(columns={"GEOID21": "geoid"})
+        acs1_df["year"] = 2021
+        a1df = (
+            acs1_df.groupby(["geoid", "year"])["B28002_001E"]
             .agg("min")
             .to_frame()
             .reset_index()
         )
-        bdf = bdf.rename(columns={"year_parsed": "year"})
-
-        acs_df = pd.read_csv(acs_f, dtype={"GEOID21": object})
-        acs_df = acs_df.rename(columns={"GEOID21": "geoid"})
-        acs_df["year"] = 2021
-        adf = (
-            acs_df.groupby(["geoid", "year"])["B19013_001E"]
-            .agg("min")
-            .to_frame()
-            .reset_index()
-        )
-        adf = adf[adf["B19013_001E"] > 0]
-        adf[
+        a1df = a1df[a1df["B28002_001E"] > 0]
+        a1df[
             "year"
-        ] = 2023  # TBD should be adjusted for inflation for moving forward in time?
+        ] = 2022  # TBD should be adjusted for inflation for moving forward in time?
 
-        bg_df = pd.merge(adf, bdf, on=["geoid", "year"])
-        bg_df["measure"] = MEASURE_NAME
-        bg_df["value"] = (
-            (bg_df["price"] * 12) / bg_df["B19013_001E"] * 100
-        )  # Multiplpy by 12 because it is price per month
-        bg_df = bg_df.reindex(settings.STANDARD_COLS, axis="columns")
-        bg_df["region_type"] = "block_group"
+        acs13_df = pd.read_csv(acs13_f, dtype={"GEOID21": object})
+        acs13_df = acs13_df.rename(columns={"GEOID21": "geoid"})
+        acs13_df["year"] = 2021
+        a13df = (
+            acs13_df.groupby(["geoid", "year"])["B28002_013E"]
+            .agg("min")
+            .to_frame()
+            .reset_index()
+        )
+        a13df = a13df[a13df["B28002_013E"] > 0]
+        a13df[
+            "year"
+        ] = 2022  # TBD should be adjusted for inflation for moving forward in time?
+        bg_df = pd.merge(a1df, a13df, on=["geoid", "year"])
+        bg_df["upload_prob"] = bg_df["geoid"].apply(
+            lambda x: calculate_cpdf(
+                ookla_df[ookla_df["geoid"].str[: len(x)] == x], "avg_u_mbps", 20
+            )
+        )
+        bg_df["download_prob"] = bg_df["geoid"].apply(
+            lambda x: calculate_cpdf(
+                ookla_df[ookla_df["geoid"].str[: len(x)] == x], "avg_d_mbps", 100
+            )
+        )
 
-        # Generating summaries at a census tract level
         ct_df = generate_higher_geo(bg_df, 11)
         c_df = generate_higher_geo(bg_df, 5)
 
-        fdf = pd.concat([c_df, ct_df, bg_df])
+        bdf = bg_df.copy()
+        bdf["measure"] = MEASURE_NAME
+        bdf["value"] = (bdf["upload_prob"] * bdf["download_prob"]) * 100
+        bdf = bdf.reindex(settings.STANDARD_COLS, axis="columns")
+        bdf["region_type"] = "block_group"
+        fdf = pd.concat([c_df, ct_df, bdf])
         fdf = fdf.reindex(settings.STANDARD_COLS, axis="columns")
-        export_filepath = os.path.join(export_dir, acs_f.name)
+        export_filepath = os.path.join(export_dir, acs1_f.name)
         pbar.set_description("Exporting to: %s" % export_filepath)
         assert not any(fdf["geoid"].isnull())  # Sanity check
         fdf.to_csv(export_filepath, index=False)
